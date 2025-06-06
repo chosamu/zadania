a541bb96d837596bc09a4d1c7759a541cb885303
chosamu <vvverraa08@gmail.com>
2025-06-06 11:21:00 +0300

    Add files via upload

---
+++ Новая папка/1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] ab = Console.ReadLine().Split();
+            byte a = Convert.ToByte(ab[0]);
+            byte b = Convert.ToByte(ab[1]);
+
+            Console.WriteLine(a + b);
+
+        }
+    }
+}
